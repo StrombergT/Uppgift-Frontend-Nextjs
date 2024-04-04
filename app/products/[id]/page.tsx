26c7879b40e6fe2f7ee6c371fd4cb8ed1b7c4067
StrombergT <tpr.stromberg@gmail.com>
2024-04-04 09:49:19 +0200

    add datocms query and testing with console.log to fetch a product

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,46 @@
+import request from "@/app/lib/datocms";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const ProductPage = async () => {
+  const query = `
+    query Productpage {
+        product {
+          name
+          id
+          price
+          description {
+            value
+          }
+          mainImage {
+            responsiveImage(imgixParams: {fit: crop, auto: format}) {
+              src
+              srcSet
+              width
+              height
+            }
+          }
+          alternativeImages {
+            responsiveImage {
+              src
+              srcSet
+              width
+              height
+            }
+          }
+        }
+      }
+    `;
+
+  const data: any = await request({
+    query,
+    preview: false,
+  });
+
+  console.log(data);
+
+  const product = data?.productpage;
+
+  return <div>Product page</div>;
 };
 
-export default page;
+export default ProductPage;
