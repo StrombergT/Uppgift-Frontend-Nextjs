62c6d887cffba01bca58793b08349ae14822d817
StrombergT <tpr.stromberg@gmail.com>
2024-04-07 23:24:00 +0200

    add new design to navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,23 +5,30 @@ import { BsBag } from "react-icons/bs";
 const Nav = () => {
   return (
     <>
-      <div className="w-full h-20 bg-[#f1f1f1] uppercase">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            <Link href={"/"}>LOGO</Link>
-            <ul className="hidden md:flex gap-x-6">
-              <li>
-                <Link href={"/products"}>
-                  <p>Products</p>
-                </Link>
-              </li>
-              <li>
-                <Link href={"/about"}>
-                  <p>About</p>
-                </Link>
-              </li>
-            </ul>
-            <BsBag className="text-2xl cursor-pointer" />
+      <div className="bg-white shadow-lg sticky top-0 py-4 z-40">
+        <div className="container mx-auto flex justify-between items-center">
+          <Link href={"/"} className="hover: text-current">
+            <h1 className="text-[26px]">
+              <span className="text-red-600 font-semibold">C</span>onsid
+            </h1>
+          </Link>
+          <ul className="flex itemx-center gap-[26px]">
+            <li>
+              <Link href={"/products"}>
+                <p>Products</p>
+              </Link>
+            </li>
+            <li>
+              <Link href={"/about"}>
+                <p>About</p>
+              </Link>
+            </li>
+          </ul>
+          <div className="relative">
+            <BsBag className="text-[26px] cursor-pointer" />
+            <div className="bg-red-500 w-[18px] h-[18px] absolute -right-1 -bottom-1 rounded-full text-white flex items-center justify-center text-sm font-medium">
+              3
+            </div>
           </div>
         </div>
       </div>
