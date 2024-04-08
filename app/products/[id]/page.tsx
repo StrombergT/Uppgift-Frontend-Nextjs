5b7cea7b0e98ac51f93d1c5ef304e76b14d7d6b8
StrombergT <tpr.stromberg@gmail.com>
2024-04-08 23:25:16 +0200

    updated styling and implement of AddToCart

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,9 @@
+import AddToCart from "@/src/components/AddToCart";
 import request from "@/src/lib/datocms";
 import { Product } from "@/src/types";
+import Link from "next/link";
 import { Image, StructuredText } from "react-datocms";
+import { CgChevronLeft } from "react-icons/cg";
 
 const ProductPage = async ({ params }: { params: { id: string } }) => {
   const query = `
@@ -41,22 +44,28 @@ const ProductPage = async ({ params }: { params: { id: string } }) => {
   return (
     <div className="container mx-auto p-4 py-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <div className="mb-4">
+        <div>
           <Image
             data={product.mainImage.responsiveImage}
             className="rounded-xl w-full sm:w-auto"
           />
         </div>
+
         <div className="sm:col-span-1">
-          <h2 className="text-2xl font-bold mb-2">{product.name}</h2>
+          <Link
+            href="/products"
+            className="uppercase flex items-center gap-2 font-semibold"
+          >
+            <CgChevronLeft size={20} />
+            Back to products
+          </Link>
+          <h2 className="text-4xl font-bold mx-auto my-8">{product.name}</h2>
           <StructuredText data={product.description.value} />
           <h3 className="text-gray-700 mt-2 text-2xl font-bold">
             ${product.price}
           </h3>
           <div>
-            <button className="bg-[#0c2d48] text-white py-4 px-8 uppercase mt-4">
-              Add to cart
-            </button>
+            <AddToCart />
           </div>
         </div>
         <div className="sm:col-span-1 grid grid-cols-3 gap-4">
