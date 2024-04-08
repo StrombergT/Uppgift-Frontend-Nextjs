55136233205d62a428430ac2519acbe48da59146
StrombergT <tpr.stromberg@gmail.com>
2024-04-08 23:19:19 +0200

    added new style in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -33,15 +33,16 @@ const Aboutpage = async () => {
   return (
     <div className="container mx-auto p-4 py-8">
       {page && (
-        <>
-          <div className="w-full mx-auto">
-            <div className="mb-4">
-              <Image data={page.mainImage.responsiveImage} className="mb-4" />
-              <h2 className="text-2xl font-bold mb-2">{page.title}</h2>
-              <StructuredText data={page.content.value} />
-            </div>
+        <div className="max-w-6xl mx-auto bg-white shadow-lg rounded-lg p-6">
+          <Image
+            data={page.mainImage.responsiveImage}
+            className="mb-4 rounded-lg"
+          />
+          <h2 className="text-3xl font-bold mb-4">{page.title}</h2>
+          <div className="text-lg mb-6">
+            <StructuredText data={page.content.value} />
           </div>
-        </>
+        </div>
       )}
     </div>
   );
