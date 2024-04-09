cafbd1e6a1ec60e5ff03908620cf0730dca39125
StrombergT <tpr.stromberg@gmail.com>
2024-04-09 16:10:54 +0200

    first layout for cartitem

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Product } from "../types";
+import { Image } from "react-datocms";
+import { FaTrash } from "react-icons/fa";
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+type CartItemProps = {
+  product: Product;
+};
+
+const CartItem = ({ product }: CartItemProps) => {
+  return (
+    <div className="flex justify-between items-center my-2 border-b border-gray-200 pb-2">
+      <div className="w-20 h-20 mr-4">
+        <Image data={product.mainImage.responsiveImage} />
+      </div>
+      <div className="flex flex-col">
+        <h3 className="font-semibold">{product.name}</h3>
+        <p className="text-gray-500">
+          ${product.price} <span> x {product.quantity}</span>
+        </p>
+      </div>
+      <div>
+        <button>
+          <FaTrash />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
