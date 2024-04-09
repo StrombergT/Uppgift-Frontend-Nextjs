9aec0af5818d1ad9d57484bf351304ab9897bff4
StrombergT <tpr.stromberg@gmail.com>
2024-04-09 10:22:08 +0200

    implemented shoppingCart in navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,30 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { BsBag } from "react-icons/bs";
+import ShoppingCart from "./ShoppingCart";
 
 const Nav = () => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
+  const closeCart = () => {
+    setIsCartOpen(false);
+  };
+
   return (
     <>
       <div className="bg-white shadow-lg sticky top-0 py-4 z-40">
         <div className="container mx-auto flex justify-between items-center">
-          <Link href={"/"} className="hover: text-current">
+          <Link href={"/"} className="hover:text-current">
             <h1 className="text-[26px]">
               <span className="text-red-600 font-semibold">C</span>onsid
             </h1>
           </Link>
-          <ul className="flex itemx-center gap-[26px]">
+          <ul className="flex items-center gap-[26px]">
             <li>
               <Link href={"/products"}>
                 <p>Products</p>
@@ -25,13 +37,22 @@ const Nav = () => {
             </li>
           </ul>
           <div className="relative">
-            <BsBag className="text-[26px] cursor-pointer" />
+            <button onClick={toggleCart}>
+              <BsBag className="text-[26px] cursor-pointer" />
+            </button>
             <div className="bg-red-500 w-[18px] h-[18px] absolute -right-1 -bottom-1 rounded-full text-white flex items-center justify-center text-sm font-medium">
               3
             </div>
           </div>
         </div>
       </div>
+      <div
+        className={`fixed inset-0 bg-black opacity-50 z-40 ${
+          isCartOpen ? "visible" : "invisible"
+        }`}
+        onClick={closeCart}
+      ></div>
+      <ShoppingCart isOpen={isCartOpen} closeCart={closeCart} />
     </>
   );
 };
