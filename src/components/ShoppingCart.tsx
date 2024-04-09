466b63192cacc41f2d12b565d4cc5793b897f9c7
StrombergT <tpr.stromberg@gmail.com>
2024-04-09 10:21:26 +0200

    created shoppingCart sidebar

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const ShoppingCart = () => {
-  return <div>ShoppingCart</div>;
+type ShoppingCartProps = {
+  isOpen: boolean;
+  closeCart: () => void;
+};
+
+const ShoppingCart = ({ isOpen, closeCart }: ShoppingCartProps) => {
+  return (
+    <div
+      className={`fixed top-0 right-0 h-full w-80 bg-white z-50 transform transition-transform duration-300 ${
+        isOpen ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <div className="p-8">
+        <h1 className="text-lg font-bold mb-4">Cart</h1>
+        <hr />
+      </div>
+      <button
+        className="absolute top-2 right-2 text-gray-600"
+        onClick={closeCart}
+      >
+        Close
+      </button>
+    </div>
+  );
 };
 
 export default ShoppingCart;
