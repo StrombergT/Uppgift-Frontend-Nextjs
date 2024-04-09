0fb0ae0efe560005f4f8d0e4060e36fd9e910402
StrombergT <tpr.stromberg@gmail.com>
2024-04-09 16:10:06 +0200

    updated shoppingcart to handle totalprice and item quantity

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,4 +1,10 @@
+"use client";
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { cartState } from "../recoil";
+import { Product } from "../types";
+import CartItem from "./CartItem";
+import { IoMdClose } from "react-icons/io";
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -6,6 +12,11 @@ type ShoppingCartProps = {
 };
 
 const ShoppingCart = ({ isOpen, closeCart }: ShoppingCartProps) => {
+  const cartItems = useRecoilValue<Product[]>(cartState);
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.quantity * item.price,
+    0
+  );
   return (
     <div
       className={`fixed top-0 right-0 h-full w-80 bg-white z-50 transform transition-transform duration-300 ${
@@ -15,12 +26,16 @@ const ShoppingCart = ({ isOpen, closeCart }: ShoppingCartProps) => {
       <div className="p-8">
         <h1 className="text-lg font-bold mb-4">Cart</h1>
         <hr />
+        {cartItems.map((item) => (
+          <CartItem product={item} key={item.id} {...item} />
+        ))}
       </div>
+      <div className="p-4"> Total Price: ${totalPrice}</div>
       <button
         className="absolute top-2 right-2 text-gray-600"
         onClick={closeCart}
       >
-        Close
+        <IoMdClose />
       </button>
     </div>
   );
