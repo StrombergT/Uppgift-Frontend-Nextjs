75e9162d824b22842897ab89b4dd6933b61f1bba
StrombergT <tpr.stromberg@gmail.com>
2024-04-10 14:47:08 +0200

    add useEffect to get total price if shoppingcart has more than one item

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useRecoilValue } from "recoil";
-import { cartState } from "../recoil";
-import { Product } from "../types";
+import { cartState, getCartValue } from "../recoil/state/atoms";
 import CartItem from "./CartItem";
 import { IoMdClose } from "react-icons/io";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -12,45 +12,55 @@ type ShoppingCartProps = {
 };
 
 const ShoppingCart = ({ isOpen, closeCart }: ShoppingCartProps) => {
-  const cartItems = useRecoilValue<Product[]>(cartState);
+  const cartItems = useRecoilValue(cartState);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  const totalPrice = cartItems.reduce(
-    (total, item) => total + item.quantity * item.price,
-    0
-  );
+  useEffect(() => {
+    let tempTotal = getCartValue(cartItems);
+    setTotalPrice(tempTotal);
+  }, [cartItems]);
 
   return (
     <div
-      className={`fixed top-0 right-0 h-full w-80 bg-white z-50 transform transition-transform duration-300 ${
+      className={`fixed top-0 right-0 h-[70vh] w-80 bg-white z-50 transform transition-transform duration-300 rounded-lg mt-0 ${
         isOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
-      <div className="p-8">
-        <h1 className="text-lg font-bold mb-4">Cart</h1>
+      <div className="p-8 justify-between items-center">
+        <h1 className="text-lg font-bold mb-4 uppercase text-center">
+          Shopping Cart
+        </h1>
         <hr />
         {cartItems.length === 0 ? (
           <p className="text-center text-gray-500">Shopping cart is empty</p>
         ) : (
           cartItems.map((item) => (
-            <CartItem product={item} key={item.id} {...item} />
+            <CartItem
+              product={item}
+              key={item.id}
+              {...item}
+              removeButton={true}
+            />
           ))
         )}
       </div>
       {cartItems.length > 0 && (
         <>
-          <div className="p-4"> Total Price: ${totalPrice}</div>
           <div className="absolute bottom-10 left-0 right-0">
+            <div className="p-4 font-primary font-semibold text-xl justify-center text-center">
+              Total Price: ${totalPrice}
+              <hr />
+            </div>
             <Link href="/checkout">
-              <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-full py-3 px-6 w-full ml-5 max-w-[280px]">
-                {" "}
-                Checkout
+              <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-full py-3 px-6 w-full ml-5 max-w-[280px] uppercase mt-5">
+                Buy
               </button>
             </Link>
           </div>
         </>
       )}
       <button
-        className="absolute top-2 right-2 text-gray-600"
+        className="absolute top-4 right-3 text-gray-600 "
         onClick={closeCart}
       >
         <IoMdClose />
