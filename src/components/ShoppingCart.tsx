076179ba2fe5270426f9825f970b82936fdc5482
StrombergT <tpr.stromberg@gmail.com>
2024-04-10 09:08:15 +0200

    create checkoutpage and implement link route to checkout from shoppingcart

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React from "react";
 import { useRecoilValue } from "recoil";
 import { cartState } from "../recoil";
 import { Product } from "../types";
 import CartItem from "./CartItem";
 import { IoMdClose } from "react-icons/io";
+import Link from "next/link";
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -13,10 +13,12 @@ type ShoppingCartProps = {
 
 const ShoppingCart = ({ isOpen, closeCart }: ShoppingCartProps) => {
   const cartItems = useRecoilValue<Product[]>(cartState);
+
   const totalPrice = cartItems.reduce(
     (total, item) => total + item.quantity * item.price,
     0
   );
+
   return (
     <div
       className={`fixed top-0 right-0 h-full w-80 bg-white z-50 transform transition-transform duration-300 ${
@@ -26,11 +28,27 @@ const ShoppingCart = ({ isOpen, closeCart }: ShoppingCartProps) => {
       <div className="p-8">
         <h1 className="text-lg font-bold mb-4">Cart</h1>
         <hr />
-        {cartItems.map((item) => (
-          <CartItem product={item} key={item.id} {...item} />
-        ))}
+        {cartItems.length === 0 ? (
+          <p className="text-center text-gray-500">Shopping cart is empty</p>
+        ) : (
+          cartItems.map((item) => (
+            <CartItem product={item} key={item.id} {...item} />
+          ))
+        )}
       </div>
-      <div className="p-4"> Total Price: ${totalPrice}</div>
+      {cartItems.length > 0 && (
+        <>
+          <div className="p-4"> Total Price: ${totalPrice}</div>
+          <div className="absolute bottom-10 left-0 right-0">
+            <Link href="/checkout">
+              <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-full py-3 px-6 w-full ml-5 max-w-[280px]">
+                {" "}
+                Checkout
+              </button>
+            </Link>
+          </div>
+        </>
+      )}
       <button
         className="absolute top-2 right-2 text-gray-600"
         onClick={closeCart}
