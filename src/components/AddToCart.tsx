e666a916760267dd00f1133a3ba9cd175547a6de
StrombergT <tpr.stromberg@gmail.com>
2024-04-14 22:11:44 +0200

    created ShoppingCartUpdateProducts hook and implemented it in AddToCart

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,62 +1,23 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { Product } from "../types";
-import { useShoppingCartUpdateProducts } from "../hooks/useShoppingCartUpdateProduct";
+import { useShoppingCartUpdateProducts } from "../hooks/useShoppingCartUpdateProducts";
 
-interface AddToCartProps {
+type AddToCartProps = {
   product: Product;
-}
+};
 
 const AddToCart = ({ product }: AddToCartProps) => {
-  const [count, setCount] = useState(0);
   const updateProducts = useShoppingCartUpdateProducts();
-  const [errorMessage, setErrorMessage] = useState<string>("");
-
-  const handleAddToCart = () => {
-    if (count === 0) {
-      setErrorMessage("You must choose how many products.");
-      setTimeout(() => {
-        setErrorMessage("");
-      }, 3000);
-      return;
-    }
-
-    const newItem = { ...product, quantity: count };
-    updateProducts(newItem);
-    setCount(0);
-  };
 
   return (
     <div className="container mx-auto flex items-center mt-4">
-      <div className="bg-[#e5e5e5] rounded-xl py-2 px-2 flex items-center space-x-2 sm:space-x-4">
-        <button
-          onClick={() => setCount(count - 1 >= 0 ? count - 1 : 0)}
-          className="px-3 py-1 rounded-md hover:bg-gray-300 focus:outline-none"
-        >
-          -
-        </button>
-        <span className="text-lg font-bold">{count}</span>
-
-        <button
-          onClick={() => setCount(count + 1)}
-          className="px-3 py-1 rounded-md hover:bg-gray-300 focus:outline-none"
-        >
-          +
-        </button>
-      </div>
       <button
-        onClick={handleAddToCart}
+        onClick={() => updateProducts(product)}
         className="ml-4 bg-[#0c2d48] rounded-md text-white py-2 px-4 sm:px-8 uppercase"
       >
         Add to cart
       </button>
-      <div
-        className={`${
-          errorMessage ? "block" : "hidden"
-        } text-red-500 transition duration-500 ease-in-out ml-5 border-2 border-red-500 rounded-md px-2 py-2`}
-      >
-        {errorMessage}
-      </div>
     </div>
   );
 };
---
+++ src/hooks/useShoppingCartUpdateProducts.ts
@@ -0,0 +1,24 @@
+import { useSetRecoilState } from "recoil";
+import { cartState } from "../recoil/state/atoms";
+import { Product } from "../types";
+
+export const useShoppingCartUpdateProducts = () => {
+  const setShoppingCartProducts = useSetRecoilState(cartState);
+
+  return (product: Product): void => {
+    setShoppingCartProducts((updateProducts) => {
+      const existingItem = updateProducts.find(
+        (item) => item.id === product.id
+      );
+      if (existingItem) {
+        return updateProducts.map((p) =>
+          p.id === product.id
+            ? { ...p, quantity: p.quantity + (product.quantity || 1) }
+            : p
+        );
+      } else {
+        return [...updateProducts, { ...product, quantity: 1 }];
+      }
+    });
+  };
+};
