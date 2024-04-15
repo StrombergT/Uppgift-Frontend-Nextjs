161fff281ffcc28b65126de0cfe2a5fe09f52d72
StrombergT <tpr.stromberg@gmail.com>
2024-04-15 09:46:36 +0200

    recreated quantity in navbar.. maybe need to place the function in a hook instead

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,34 +3,42 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { BsBag } from "react-icons/bs";
 import ShoppingCart from "./ShoppingCart";
+import { useRecoilValue } from "recoil";
+import { cartState } from "../recoil/state/atoms";
 
 const Nav = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const cartItems = useRecoilValue(cartState);
 
-  const toggleCart = () => {
-    setIsCartOpen(!isCartOpen);
-  };
+  const openCart = () => setIsCartOpen(true);
+  const closeCart = () => setIsCartOpen(false);
 
-  const closeCart = () => {
-    setIsCartOpen(false);
-  };
+  const totalQuantity = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
 
   return (
-    <header className=" w-full ">
-      <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
+    <header className="w-full ">
+      <nav className=" border-gray-200 py-2.5 bg-gray-900">
         <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
           <a className="flex items-center" href="/">
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
               <span className="text-red-800 text-5xl font-bold">C</span>onsid
             </span>
           </a>
           <div className="flex items-center lg:order-2">
             <button
-              onClick={toggleCart}
-              className="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              onClick={openCart}
+              className="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium border rounded-lg sm:w-auto focus:outline-none  focus:z-10 focus:ring-4 focus:ring-gray-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700"
             >
               <BsBag className="text-[18px] text-white" />
             </button>
+            {totalQuantity > 0 && (
+              <div className="bg-red-500 w-[18px] h-[18px] absolute rounded-full text-white flex items-center justify-center text-sm font-medium mb-8">
+                {totalQuantity}
+              </div>
+            )}
           </div>
           <div className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
@@ -43,14 +51,14 @@ const Nav = () => {
               </li>
               <li>
                 <Link href={"/products"}>
-                  <p className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                  <p className="block py-2 pl-3 pr-4 border-b lg:border-0 lg:p-0 text-gray-400 lg:hover:text-white hover:bg-gray-700 hover:text-white lg:hover:bg-transparent border-gray-700">
                     Products
                   </p>
                 </Link>
               </li>
               <li>
                 <Link href={"/about"}>
-                  <p className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                  <p className="block py-2 pl-3 pr-4 text-gray-400 border-b lg:border-0 lg:p-0 lg:hover:text-white hover:bg-gray-700 hover:text-white lg:hover:bg-transparent border-gray-700">
                     About
                   </p>
                 </Link>
